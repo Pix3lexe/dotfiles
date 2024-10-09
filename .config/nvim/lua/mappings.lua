b225631d401bf9effa1ed320d2aaa2d72d5bfafb
Pix3lexe <hermanns.simon@gmail.com>
2024-10-09 15:51:19 +0200

    Updated tab mappings to switch to prev and next

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -6,5 +6,13 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+map("n", "<leader>ll", function()
+	require("nvchad.tabufline").next()
+end, { desc = "Next buffer", silent = true })
+
+map("n", "<leader>hh", function()
+	require("nvchad.tabufline").prev()
+end, { desc = "Previous buffer", silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
