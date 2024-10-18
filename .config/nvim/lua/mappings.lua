7c4555c95e06fee4843c783d1e89425a45394aba
Simon Hermanns <hermanns.simon@gmail.com>
2024-10-18 15:59:19 +0200

    Added markdown vim plugin

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,11 +8,15 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "<leader>ll", function()
-	require("nvchad.tabufline").next()
+    require("nvchad.tabufline").next()
 end, { desc = "Next buffer", silent = true })
 
 map("n", "<leader>hh", function()
-	require("nvchad.tabufline").prev()
+    require("nvchad.tabufline").prev()
 end, { desc = "Previous buffer", silent = true })
+
+map("n", "<leader>md", function()
+    vim.cmd("MarkdownPreview")
+end, { desc = "Preview Markdown file", silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,39 +1,48 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		-- event = 'BufWritePre', -- uncomment for format on save
-		config = function()
-			require("configs.conform")
-		end,
-	},
-	{ "mfussenegger/nvim-jdtls" },
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        -- event = 'BufWritePre', -- uncomment for format on save
+        config = function()
+            require("configs.conform")
+        end,
+    },
+    { "mfussenegger/nvim-jdtls" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
 
-	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("nvchad.configs.lspconfig").defaults()
-			require("configs.lspconfig")
-		end,
-	},
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/nvim-cmp" },
-	{ "L3MON4D3/LuaSnip" },
-	{
-		"nmac427/guess-indent.nvim",
-		config = function()
-			require("guess-indent").setup({})
-		end,
-	},
-	{ "github/copilot.vim", lazy = true },
-	{
-		"adelarsq/image_preview.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("image_preview").setup({})
-		end,
-	},
+    { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
+    },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/nvim-cmp" },
+    { "L3MON4D3/LuaSnip" },
+    {
+        "nmac427/guess-indent.nvim",
+        config = function()
+            require("guess-indent").setup({})
+        end,
+    },
+    { "github/copilot.vim", lazy = true },
+    {
+        "adelarsq/image_preview.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("image_preview").setup({})
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 }
