9466ff8d012212151d8b576fc5bdeb14035f5258
Simon Hermanns <hermanns.simon@gmail.com>
2024-10-23 12:35:56 +0200

    Bind <leader>lr to list LSP references

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -27,4 +27,8 @@ map("n", "<leader>dr", function()
     vim.cmd("DapContinue")
 end, { desc = "Start or continue the debugger", silent = true })
 
+map("n", "<leader>lr", function()
+    require('telescope.builtin').lsp_references()
+end, { desc = "See references to word under cursor in telescope", silent = true })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
