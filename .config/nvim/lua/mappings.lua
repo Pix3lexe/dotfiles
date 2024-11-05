60020c8d5d1bd8ec8e8d4e6f1ce8814fa34f57c8
Simon Hermanns <hermanns.simon@gmail.com>
2024-11-05 19:24:13 +0100

    Fixed base46 theme error

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,28 +7,20 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("n", "<leader>ll", function()
-    require("nvchad.tabufline").next()
-end, { desc = "Next buffer", silent = true })
-
-map("n", "<leader>hh", function()
-    require("nvchad.tabufline").prev()
-end, { desc = "Previous buffer", silent = true })
-
 map("n", "<leader>md", function()
-    vim.cmd("MarkdownPreview")
+	vim.cmd("MarkdownPreview")
 end, { desc = "Preview Markdown file", silent = true })
 
 map("n", "<leader>bp", function()
-    vim.cmd("DapToggleBreakpoint")
+	vim.cmd("DapToggleBreakpoint")
 end, { desc = "Toggle breakpoint", silent = true })
 
 map("n", "<leader>dr", function()
-    vim.cmd("DapContinue")
+	vim.cmd("DapContinue")
 end, { desc = "Start or continue the debugger", silent = true })
 
 map("n", "<leader>lr", function()
-    require('telescope.builtin').lsp_references()
+	require("telescope.builtin").lsp_references()
 end, { desc = "See references to word under cursor in telescope", silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,82 +1,82 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = { "BufReadPre", "BufNewFile", "BufWritePre" },
-        -- event = 'BufWritePre', -- uncomment for format on save
-        config = function()
-            require("configs.conform")
-        end,
-    },
-    { "mfussenegger/nvim-jdtls" },
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile", "BufWritePre" },
+		-- event = 'BufWritePre', -- uncomment for format on save
+		config = function()
+			require("configs.conform")
+		end,
+	},
+	{ "mfussenegger/nvim-jdtls" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
-    { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-            require("configs.lspconfig")
-        end,
-    },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/nvim-cmp" },
-    { "L3MON4D3/LuaSnip" },
-    {
-        "nmac427/guess-indent.nvim",
-        config = function()
-            require("guess-indent").setup({})
-        end,
-    },
-    { "github/copilot.vim",   lazy = true },
-    {
-        "adelarsq/image_preview.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("image_preview").setup({})
-        end,
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    },
-    {
-        "mfussenegger/nvim-dap"
-    },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "mfussenegger/nvim-dap",
-        },
-        opts = {
-            handlers = {},
-        },
-    },
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = "mfussenegger/nvim-dap",
-        event = "VeryLazy",
-        config = function()
-            local dap = require("dap")
-            local dapui = require("dapui")
-            dapui.setup()
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
-        end
-    },
-    { "nvim-neotest/nvim-nio" },
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
+	{
+		"nmac427/guess-indent.nvim",
+		config = function()
+			require("guess-indent").setup({})
+		end,
+	},
+	{ "github/copilot.vim", lazy = true },
+	{
+		"adelarsq/image_preview.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("image_preview").setup({})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"mfussenegger/nvim-dap",
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = {
+			handlers = {},
+		},
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = "mfussenegger/nvim-dap",
+		event = "VeryLazy",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{ "nvim-neotest/nvim-nio" },
 }
