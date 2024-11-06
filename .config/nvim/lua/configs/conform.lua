493cd9646d59bf1d3aa55e73cb4b7adca1f3414a
Simon Hermanns <hermanns.simon@gmail.com>
2024-11-06 19:16:48 +0100

    Removed unnecessary clang-14 format if in petrack dir, did not really work anyway

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,39 +1,25 @@
 local conform = require("conform")
 
-conform.formatters["clang-format-14"] = {
-    command = "clang-format-14",
-}
-
-local function get_cpp_formatter()
-    local cwd = vim.loop.cwd()
-
-    if cwd:match("petrack") then
-        return "clang-format-14"
-    else
-        return "clang-format"
-    end
-end
-
 local options = {
-    formatters_by_ft = {
-        lua = { "stylua" },
-        cpp = { get_cpp_formatter() }
-        -- css = { "prettier" },
-        -- html = { "prettier" },
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		cpp = { "clang-format" },
+		-- css = { "prettier" },
+		-- html = { "prettier" },
+	},
 
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_fallback = true,
-    },
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-        conform.format({
-            lsp_fallback = true,
-            async = false,
-            timeout_ms = 500,
-        })
-    end, { desc = "Format file or range (in visual mode)" })
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+	vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		conform.format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 500,
+		})
+	end, { desc = "Format file or range (in visual mode)" }),
 }
 
 conform.setup(options)
