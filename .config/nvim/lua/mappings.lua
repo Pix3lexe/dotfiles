0f410285eba93be871dbddeb1472c461bf6b6d72
Simon Hermanns <hermanns.simon@gmail.com>
2024-11-08 19:53:02 +0100

    Modified some vertical motion mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,26 +1,27 @@
 require("nvchad.mappings")
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "<leader>md", function()
-	vim.cmd("MarkdownPreview")
+    vim.cmd("MarkdownPreview")
 end, { desc = "Preview Markdown file", silent = true })
 
 map("n", "<leader>bp", function()
-	vim.cmd("DapToggleBreakpoint")
+    vim.cmd("DapToggleBreakpoint")
 end, { desc = "Toggle breakpoint", silent = true })
 
 map("n", "<leader>dr", function()
-	vim.cmd("DapContinue")
+    vim.cmd("DapContinue")
 end, { desc = "Start or continue the debugger", silent = true })
 
 map("n", "<leader>lr", function()
-	require("telescope.builtin").lsp_references()
+    require("telescope.builtin").lsp_references()
 end, { desc = "See references to word under cursor in telescope", silent = true })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+map("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+map("n", "n", "nzzzv", { noremap = true, silent = true })
+map("n", "N", "Nzzzv", { noremap = true, silent = true })
