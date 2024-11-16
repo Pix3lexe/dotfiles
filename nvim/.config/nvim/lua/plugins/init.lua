4f03ac89f9af834a9b1cbbbccaebc92f161cd09b
Simon Hermanns <hermanns.simon@gmail.com>
2024-11-16 13:15:49 +0100

    Initial for stow

---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,146 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"lua",
+				"luadoc",
+				"printf",
+				"vim",
+				"vimdoc",
+				"c",
+				"cpp",
+				"python",
+				"markdown",
+				"json",
+				"css",
+				"html",
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile", "BufWritePre" },
+		-- event = 'BufWritePre', -- uncomment for format on save
+		config = function()
+			require("configs.conform")
+		end,
+	},
+	{ "mfussenegger/nvim-jdtls" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
+	{
+		"nmac427/guess-indent.nvim",
+		config = function()
+			require("guess-indent").setup({})
+		end,
+	},
+	{ "github/copilot.vim", lazy = true },
+	{
+		"adelarsq/image_preview.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("image_preview").setup({})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"mfussenegger/nvim-dap",
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = {
+			handlers = {},
+		},
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = "mfussenegger/nvim-dap",
+		event = "VeryLazy",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{ "nvim-neotest/nvim-nio" },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			highlight = {
+				backdrop = true,
+				matches = true,
+				priority = 5000,
+				groups = {
+					match = "FlashMatch",
+					current = "FlashCurrent",
+					backdrop = "FlashBackdrop",
+					label = "FlashLabel", -- Use default label group
+				},
+			},
+		},
+		config = function()
+			local colors = dofile(vim.g.base46_cache .. "colors")
+			vim.api.nvim_set_hl(0, "FlashLabel", { fg = colors.grey, bg = colors.red })
+		end,
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+	},
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		keys = {
+			{
+				"<leader>cc",
+				"<cmd>CodeSnap<cr>",
+				mode = "x",
+				desc = "Save selected code snapshot into clipboard",
+			},
+		},
+		opts = {
+			bg_theme = "grape",
+			watermark = "PixelHD",
+			has_breadcrumbs = true,
+		},
+	},
+}
