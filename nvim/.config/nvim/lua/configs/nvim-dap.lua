244d6abc7fc019653abcd5fb120fb10159aa6c1c
Simon Hermanns <hermanns.simon@gmail.com>
2024-12-03 21:46:13 +0100

    Revert to default dapui icons

--- nvim/.config/nvim/lua/configs/nvim-dap.lua
+++ nvim/.config/nvim/lua/configs/nvim-dap.lua
@@ -21,25 +21,7 @@ return function()
 
   -- Dap UI setup
   -- For more information, see |:help nvim-dap-ui|
-  dapui.setup {
-    -- Set icons to characters that are more likely to work in every terminal.
-    --    Feel free to remove or use ones that you like more! :)
-    --    Don't feel like these are good choices.
-    icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-    controls = {
-      icons = {
-        pause = '⏸',
-        play = '▶',
-        step_into = '⏎',
-        step_over = '⏭',
-        step_out = '⏮',
-        step_back = 'b',
-        run_last = '▶▶',
-        terminate = '⏹',
-        disconnect = '⏏',
-      },
-    },
-  }
+  dapui.setup {}
 
   -- Change breakpoint icons
   -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
