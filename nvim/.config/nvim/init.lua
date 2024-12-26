36d5c28a4095cc73e60eda622198f7a1e77ac32a
Simon Hermanns <hermanns.simon@gmail.com>
2024-12-26 14:23:47 +0100

    Add gitconfig and some other improvements

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -46,6 +46,14 @@ require('lazy').setup {
       lazy = '💤 ',
     },
   },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'netrw',
+        'netrwPlugin',
+      },
+    },
+  },
 }
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
