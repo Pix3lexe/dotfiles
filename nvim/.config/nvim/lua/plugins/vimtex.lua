a43a4b69f630a2223e972f2402ac718a91225f0a
Simon Hermanns <hermanns.simon@gmail.com>
2026-01-14 18:08:39 +0100

    Update waybar memory symbol + vimtex changes

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -7,5 +7,10 @@ return {
     vim.g.vimtex_view_forward_search_on_start = 0
     vim.g.vimtex_view_follow = 1
     vim.g.vimtex_syntax_enabled = 0
+    vim.g.vimtex_imaps_enabled = 0
+    vim.g.tex_conceal = ''
+    vim.opt.conceallevel = 0
+
+    vim.g.vimtex_compiler_method = 'latexmk'
   end,
 }
