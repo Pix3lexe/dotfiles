244333d2c37a199f0ead274ff8910120f6e2bb8d
Simon Hermanns <hermanns.simon@gmail.com>
2026-03-23 20:18:10 +0100

    fix: flavor_changer still looking inside wofi css

--- eww/.config/eww/scripts/flavor_changer.sh
+++ eww/.config/eww/scripts/flavor_changer.sh
@@ -22,13 +22,7 @@ if [ -f "$EWW_YUCK" ]; then
     sed -i "s/(defvar current_flavor \"[a-zA-Z0-9_]*\")/(defvar current_flavor \"$flavor\")/g" "$EWW_YUCK"
 fi
 
-# 2. wofi
-WOFI_CSS="$HOME/.config/wofi/style.css"
-if [ -f "$WOFI_CSS" ]; then
-    sed -i "s/@define-color flavor @\([a-zA-Z]*\);/@define-color flavor @$flavor;/g" "$WOFI_CSS"
-fi
-
-# 3. Replace in ~/.config/hypr/hyprland.conf:
+# 2. Replace in ~/.config/hypr/hyprland.conf:
 # a) env = GTK_THEME, catppuccin-mocha-[CURRENT_flavor]-standard+default
 HYPRLAND_CONF="$HOME/.config/hypr/hyprland.conf"
 if [ -f "$HYPRLAND_CONF" ]; then
