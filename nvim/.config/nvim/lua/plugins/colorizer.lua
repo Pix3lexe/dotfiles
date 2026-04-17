3ebcc76902d83c7015b27478302bc071b1cfb24b
Simon Hermanns <hermanns.simon@gmail.com>
2026-04-17 11:08:58 +0200

    chore(nvim): switch to newer nvim-colorizer

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,7 +1,5 @@
 return {
-  'norcalli/nvim-colorizer.lua',
-  event = { 'BufReadPost', 'BufNewFile' },
-  config = function()
-    require('colorizer').setup()
-  end,
+  'catgoose/nvim-colorizer.lua',
+  event = 'BufReadPre',
+  opts = {},
 }
